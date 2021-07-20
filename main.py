30f68c1ef94be511a5d36d7053c265c30b49f418
nestor <nestorcolt@hotmail.com>
2021-07-20 21:18:59 +0200

    update valid code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pprint
 import json
 
 ##############################################################################################
-
+# LOGIN AREA
 init_url = r"https://px-conf.perimeterx.net/api/v1/mobile"
 auth_type = r"https://developer.api.walmart.com/api-proxy/service/lmde/app/v4/services/auth/type"
 api_login = r"https://developer.api.walmart.com/api-proxy/service/lmde/app/v4/services/auth/login"
@@ -40,10 +40,11 @@ main_headers = \
         "user-agent": "Dalvik/2.1.0 (Linux; U; Android 5.1.1; ASUS_I001DA Build/LMY49I)",
         "wm_tenant_id": "0",
     }
+
 ##############################################################################################
 
 # init_request = requests.post(init_url, data=init_payload)
-# auth_request = requests.post(auth_type, json=auth_payload, headers=header.var)
+# auth_request = requests.post(auth_type, json=auth_payload, headers=main_headers)
 login_request = requests.post(api_login, json=login_payload, headers=main_headers)
 
 # print codes
@@ -53,3 +54,6 @@ login_request = requests.post(api_login, json=login_payload, headers=main_header
 # print(auth_request.json())
 print(login_request)
 pprint.pprint(login_request.json())
+
+##############################################################################################
+# GET OFFERS AREA
